a2d8ea47880094051fec2697ead53dd839ce5dea
Qasa INto <m78@m78dexingjirenshidemacbook-pro.local>
2018-04-02 21:59:11 +0800

    Initial Commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Pasan Premaratne on 10/22/15.
+//  Copyright © 2015 Treehouse. All rights reserved.
+//
+
+import UIKit
+import SwiftRandom
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var funFactLabel: UILabel!
+    
+    @IBOutlet weak var funFactButton: UIButton!
+    
+    let factModel = FactModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        funFactLabel.text = factModel.getRandomFact()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+ 
+    @IBAction func showFunFact() {
+//        let randomColor = ColorModel().getRandomColor()
+        let randomColor = Randoms.randomColor();
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
+        funFactLabel.text = factModel.getRandomFact()
+    }
+
+}
+
+
+
+
+
+
